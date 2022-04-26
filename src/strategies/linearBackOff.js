f91922b335805c0bd338c36adab539b816810954
SeizedThoughts <seizedthoughts@gmail.com>
2022-04-26 03:50:30 -0700

    FIX: retries value of -1 not functioning properly

--- src/strategies/linearBackOff.js
+++ src/strategies/linearBackOff.js
@@ -1,7 +1,7 @@
 const delay = require('../util/delay');
 
 function linearBackOff(config, attempt){
-    let timeout = config.minTimeout + ((attempt - 1) * (config.maxTimeout - config.minTimeout) / config.retries);
+    let timeout = config.minTimeout + ((attempt - 1) * (config.retries === -1 ? config.minTimeout : ((config.maxTimeout - config.minTimeout) / config.retries)));
     if(config.randomize) timeout += (Math.random() - 0.5) * config.minTimeout;
     return delay(timeout);
 }
